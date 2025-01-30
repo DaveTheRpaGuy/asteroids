aed060c685b92c65c80bc29c83b92b7aeceed009
davetherpaguy <dave@davetherpaguy.com>
2025-01-29 21:15:18 -0500

    player can pew pew now

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -17,10 +18,12 @@ def main():
     group_updatable = pygame.sprite.Group()
     group_drawable = pygame.sprite.Group()
     group_asteroids = pygame.sprite.Group()
+    group_shots = pygame.sprite.Group()
 
     Player.containers = (group_updatable, group_drawable)
     Asteroid.containers = (group_asteroids, group_updatable, group_drawable)
     AsteroidField.containers = (group_updatable)
+    Shot.containers = (group_shots, group_updatable, group_drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -32,6 +35,11 @@ def main():
 
         for updatable in group_updatable:
             updatable.update(dt)
+
+        for asteroid in group_asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                exit()
 
         screen.fill("black")
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -34,7 +35,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt*-1)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def check_collision(self, other_circle):
+        return super().check_collision(other_circle)
+    
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+        
